497808226d6a12721227279b1148df8a0ffd6942
Anagha Anil Kumar <164894594+AnaghaAni@users.noreply.github.com>
2025-06-10 19:42:57 +0530

    Add files via upload

---
+++ controlStatement.java
@@ -0,0 +1,16 @@
+package ControlStatement;
+
+public class controlStatement {
+
+	public static void main(String[] args) {
+		int age = 15;
+		if (age >= 18)
+			System.out.println("Adult");
+		else if(age >=12)
+			System.out.println("Teen");
+		else {
+			System.out.println("Child");
+		}
+	}
+
+}
